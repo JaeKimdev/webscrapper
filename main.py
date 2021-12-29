5a5f50252e0b17e4f97a49b203e536db86ba5093
Jae Hyung Kim <smfc.mene@gmail.com>
2021-12-29 23:04:34 +0800

    Extracting Indeed Pages

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
 import requests
+from bs4 import BeautifulSoup
 
 indeed_result = requests.get("https://au.indeed.com/jobs?q=python&l=Perth%20WA&limit=50")
 
-print(indeed_result.txt)
+indeed_soup = BeautifulSoup(indeed_result.text, "html.parser")
 
+pagination = indeed_soup.find("div", class_="pagination")
+
+links = pagination.find_all('a')
+
+pages = []
+
+for link in links:
+    pages.append(int(link.string))
+max_page = pages[-1]
